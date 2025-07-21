d846150575d877c097e7760b8dd36b04ae248195
Donjohn21 <162833577+Donjohn21@users.noreply.github.com>
2025-07-21 14:04:41 -0400

    Configurar proyecto de pruebas con xUnit y referencias

--- MaquinaCafe.Tests/UnitTest1.cs
+++ MaquinaCafe.Tests/UnitTest1.cs
@@ -1,15 +1,10 @@
-using Xunit;
-using MaquinaCafeApp.Models;
+namespace MaquinaCafe.Tests;
 
-namespace MaquinaCafe.Tests
+public class UnitTest1
 {
-    public class CafeTests
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Azucarero_Inicializado_Vacio()
-        {
-            var azucarero = new Azucarero();
-            Assert.Equal(0, azucarero.Cantidad);
-        }
+
     }
 }
